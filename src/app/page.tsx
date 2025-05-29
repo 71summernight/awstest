6845477f0ccdd5e9f5ed5bccc58e70e805090c80
hyunjinkim <71summernight@naver.com>
2025-05-29 15:49:54 +0900

    배포테스트2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
-            배포테스트 
+            배포테스트 2
           </li>
         </ol>
 
